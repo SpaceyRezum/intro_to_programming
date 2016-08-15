400cb37b3b42b836d5749a06cb9cca5ee1e69961
Spacey_Rezum <SpaceyRezum@users.noreply.github.com>
2016-08-15 22:29:04 +0200

    Modification to load the map when clicking on tab

---
+++ resumeBuilder.js
@@ -0,0 +1,292 @@
+var bio = {
+    "name": "Alexis Bellet",
+    "age": 26,
+    "position": "Trilingual Digital Marketer Specialized in Search Marketing",
+    "contacts": {
+        "email": "info@alexis-bellet.com",
+        "phone": "+336/42/36/77/37",
+        "twitter": "https://twitter.com/Alexis_Bellet",
+        "github": "https://github.com/SpaceyRezum",
+        "linkedin": "https://fr.linkedin.com/in/alexisbellet",
+        "location": "Toronto, Canada"
+    },
+    "welcomeMessage": "Hi there, my name is Alexis and this is my online resume! Welcome!",
+    "biopic": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAJVAAAAJGNjZWE2N2EwLWJlMDYtNDEyNS1iYjc3LWJkOTM0ZDliNTJlZA.jpg",
+    "skills": [
+        "Trilingual (EN-FR-DE)",
+        "Search Marketing",
+        "Social Media Marketing",
+        "Agile Management",
+        "Marketing Automation",
+        "HTML/CSS/JS"
+    ]
+};
+
+var work_experience = {
+    "jobs": [{
+        "name": "ReportLinker",
+        "position": "Search Engine Optimizer",
+        "dates": "03/15 - 07/16",
+        "location": "Lyon, France",
+        "description": "In charge of increasing the organic acquisition (SEO) at Ubiquick SAS (Reportlinker.com & Reportbuyer.com), my main activities covered:" +
+        "<ul><li>Reportlinker's internal linking improvement</li>" +
+        "<li>Content management to improve the website sections' semantic search relevance</li>" +
+        "<li>Organic search performances monitoring</li>" +
+        "<li>Screening the online search fast-evolving environment to remain aware of latest updates</li>" +
+        "<li>Technical quick-fixes to constantly respect Search Engines' guidelines</li></ul>",
+        "website": "http://www.reportlinker.com/"
+    }, {
+        "name": "Ben & Jerry's D-A-CH",
+        "position": "Brand Building (Paid Internship)",
+        "dates": "03/14 - 09/14",
+        "location": "Hamburg, Germany",
+        "description": "I interned at Ben & Jerry's for 6 months and was supporting the brand building team in their on & off-line promotion activities" +
+      	"in 3 german-speaking markets (Germany, Austria & Swtizerland). My main tasks were:" +
+      	"<ul><li>Project management (music festival organization) & external agencies coordination</li>" +
+        "<li>Management of the brand's online communities (social media & websites)</li>" +
+        "<li>Offline and Online performance tracking (from regional sales to event-related web traffic)</li>" +
+        "<li>Development of new marketing solutions</li></ul>",
+        "website": "http://www.benjerry.de/"
+    }, {
+        "name": "POGED.com",
+        "position": "Country Manager France (Part Time)",
+        "dates": "11/12 - 01/14",
+        "location": "Hamburg, Germany",
+        "description": "My tasks were essentially to manage POGED France's community and help grow the company's revenues in French-speaking countries:" +
+        "<ul><li>Content management for POGED France</li>" +
+        "<li>Paid search campaign optimization</li>" +
+        "<li>Website optimization (http://fr.poged.com)</li>" +
+        "<li>French speaking community management on social media</li></ul>",
+        "website": "http://us.poged.com/"
+    }, {
+        "name": "Zalando",
+        "position": "Content Marketing & SEO (Paid Internship)",
+        "dates": "10/11 - 09/12",
+        "location": "Berlin, Germany",
+        "description": "During a 10-month internship, I have helped my team optimize Zalando's french-speaking websites. My main tasks were:" +
+        "<ul><li>Websites oerformance analysis</li>" +
+        "<li>Content management</li>" +
+        "<li>Link building</li>" +
+        "<li>New intern training & coaching</li></ul>",
+        "website": "https://www.zalando.fr/"
+    }]
+};
+
+var education = {
+    "schools": [{
+        "name": "Hamburg University",
+        "degree": "Masters",
+        "major": "International Business Administration",
+        "yearsAttended": "2012-2015 (including 6-month internship)",
+        "city": "Hamburg, Germany",
+        "website": "https://www.wiso.uni-hamburg.de/studienbuero-sozialoekonomie/studiengaenge/ma-mibas.html"
+    }, {
+        "name": "Nottingham Trent University",
+        "degree": "BA",
+        "major": "Marketing Management",
+        "yearsAttended": "2010-2011",
+        "city": "Notthingham, United Kingdom",
+        "website": "https://www.ntu.ac.uk/study-and-courses/courses/find-your-course/business/ug/2017-18/marketing"
+    }, {
+        "name": "IUT II Grenoble",
+        "degree": "2-year Business Degree",
+        "major": "Business & Marketing",
+        "yearsAttended": "2008-2010",
+        "city": "Grenoble, France",
+        "website": "http://www.iut2.upmf-grenoble.fr/international-/study-at-iut2-grenoble/i-wish-to-study-at-iut2/business-and-marketing-278277.htm"
+    }],
+    "onlineCourses": [{
+        "title": "Front End Developer - Nanodegree",
+        "school": "Udacity",
+        "dates": "August 2016 - in Progress",
+        "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+    }, {
+        "title": "Introduction to Programming - Nanodegree",
+        "school": "Udacity",
+        "dates": "June-July 2016",
+        "url": "https://www.udacity.com/course/intro-to-programming-nanodegree--nd000"
+    }, {
+        "title": "CSS Fundamentals",
+        "school": "Lynda.com",
+        "dates": "2015",
+        "url": "https://www.lynda.com/CSS-tutorials/CSS-Fundamentals/417645-2.html"
+    }, {
+        "title": "HTML Essential Training",
+        "school": "Lynda.com",
+        "dates": "2015",
+        "url": "https://www.lynda.com/HTML-tutorials/HTML-Essential-Training/170427-2.html"
+    }]
+};
+
+var projects = {
+    "projects": [{
+        "title": "Design of ReportLinker's blog: Insight",
+        "dates": "Spring 2016",
+        "url": "http://www.reportlinker.com/insight/",
+        "description": "In order to have a support platform for content marketing purposes," +
+        "ReportLinker needed a new design for its blog. With the help of a graphic designer and Wordpress" +
+        "I redesigned the entire blog and added functionalities to offer a great user experience and satisfy our marketing needs.",
+        "images": [
+            "http://www.reportlinker.com/insight/wp-content/uploads/2016/07/Housing_American-dream.png",
+            "http://www.reportlinker.com/insight/wp-content/uploads/2016/07/Top-10-largest-hot-drinks-market-by-value-and-growth-2014-500x292.png"
+        ]
+    }, {
+        "title": "Organization of 2014 Ben & Jerry's Taste & Tunes tour in D-A-CH",
+        "dates": "March - August 2014",
+        "url": "http://www.benjerry.de/blog/taste--tunes-tourfilm-2014",
+        "description": "Part of the organization team for the Ben & Jerry's 2014 Taste & Tune tour" +
+         "which brought bands and free ice cream to 28 cities accross Germany, Austria & Switzerland (D-A-CH).",
+        "images": [
+            "http://www.coolbrandz.com/wp-content/uploads/2014/06/TnT_Plakat_A3_Zuerich-k-212x300.jpg",
+            "http://2.bp.blogspot.com/-KDGyzaiT7-I/U-NgCvOvkhI/AAAAAAAAOKE/ojWCNm_hIjY/s1600/Luloveshandmade-Ben+&+Jerry%27s-Taste+&+Tunes+Tour+2014-1.JPG"
+        ]
+    }]
+};
+
+bio.display = function() {
+    // Insertion of the "quick intro" section with pictures and contact details
+    var formattedHeaderName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedHeaderRole = HTMLheaderRole.replace("%data%", bio.position);
+    var formattedHeaderPic = HTMLbioPic.replace("%data%", bio.biopic);
+    $("#header").prepend(formattedHeaderRole).prepend(formattedHeaderName);
+    $("#header").append("<div id='belowDetails'></div>");
+    $("#belowDetails").append(formattedHeaderPic);
+    $("#belowDetails").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+
+    // Insertion of all the contact details in the ul with #topContacts
+    $("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+    $("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.phone));
+    var twitterHandle = (bio.contacts.twitter).replace("https://twitter.com/", "@");
+    $("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter).replace("%handle%", twitterHandle));
+    $("#topContacts").append(HTMLlinkedin.replace("%data%", bio.contacts.linkedin).replace("%handle%", bio.name));
+    var githubHandle = (bio.contacts.github).replace("https://github.com/", "");
+    $("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github).replace("%handle%", githubHandle));
+    $("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+
+    // Insertion of the skill @ glance section
+    $("#welcome_msg").append(HTMLskillsStart);
+    for (var i = 0; i < bio.skills.length; i++) {
+        var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills-ul").append(formattedSkills);
+    }
+};
+
+work_experience.display = function() {
+    // Insertion of the work experience section
+    (work_experience.jobs).forEach(function(job) {
+        $("#workExperience").append(HTMLworkStart);
+        var formattedJobTitle = HTMLworkEmployer.replace("%data%", job.name).replace("#", job.website) + HTMLworkTitle.replace("%data%", job.position);
+        $(".work-entry:last").append(formattedJobTitle);
+        $(".work-entry:last").append(HTMLworkDates.replace("%data%", job.dates));
+        $(".work-entry:last").append(HTMLworkLocation.replace("%data%", job.location));
+        $(".work-entry:last").append(HTMLworkDescription.replace("%data%", job.description));
+    });
+};
+
+education.display = function() {
+    // Insertion of the School + Online courses section
+    $("#education").append(HTMLuniversities);
+    $("#education").append(HTMLonlineClasses);
+    (education.schools).forEach(function(school) {
+        $("#universities").append(HTMLschoolStart);
+        var formattedSchool = HTMLschoolName.replace("%data%", school.name).replace("#", school.website) + HTMLschoolDegree.replace("%data%", school.degree) + HTMLschoolMajor.replace("%data%", school.major);
+        $(".education-entry:last").append(formattedSchool);
+        $(".education-entry:last").append(HTMLschoolDates.replace("%data%", school.yearsAttended));
+        $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", school.city));
+    });
+    (education.onlineCourses).forEach(function(school) {
+        $("#online_courses").append(HTMLschoolStart);
+        var formattedSchool = HTMLschoolName.replace("%data%", school.title).replace("#", school.url) + "</a>";
+        $(".education-entry:last").append(formattedSchool);
+        $(".education-entry:last").append(HTMLschoolDates.replace("%data%", school.dates));
+        $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", school.school));
+    });
+};
+
+projects.display = function() {
+    // Insertion of the projects section
+    (projects.projects).forEach(function(project) {
+        $("#projects").append(HTMLprojectStart);
+        $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", project.title).replace("#", project.url));
+        $(".project-entry:last").append(HTMLprojectDates.replace("%data%", project.dates));
+        $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", project.description));
+	      if (project.images.length >= 1) {
+	      	$(".project-entry:last").append(HTMLprojectImageStart);
+		      if (project.images.length < 2) {
+	          $(".project_images:last").append(HTMLprojectImage.replace("%data%", project.images));
+	        } else {
+	          (project.images).forEach(function(image) {
+	            $(".project_images:last").append(HTMLprojectImage.replace("%data%", image));
+	          });
+	        }
+	      }
+    });
+};
+
+/*
+For the navbar and tabs content I decided to create via bootstrap, I create the following object
+to store css-selector of various html elements and their written form if we need to write them in title or in text format.
+The contact form section has been added for potential future use. No code has been written for it so far.
+*/
+var resumeSections = {
+	"work": {
+		"id": "workExperience",
+		"writtenForm": "Work Experience"
+	},
+	"projects": {
+		"id": "projects",
+		"writtenForm": "My Projects"
+	},
+	"education": {
+		"id": "education",
+		"writtenForm": "Universities & Online Courses"
+	},
+	"googleMap": {
+		"id": "mapDiv",
+		"writtenForm": "Where I've Lived & Worked"
+	},
+	"contactForm": {
+		"id": "get-in-touch",
+		"writtenForm": "Let's Get In Touch"
+	}
+};
+
+function displayNavBarBelowHeader() {
+	// This function creates the navbar and allocates IDs to the different section.
+	$("#content").append(navBarStart);
+	var formattedNavBarActiveElement = navBarActiveElement.replace(new RegExp(/%data%/g), resumeSections.work.id).replace("%title%", resumeSections.work.writtenForm);
+	$("#navbar_ul").append(formattedNavBarActiveElement);
+	$("#navbar_ul").append(navBarElement.replace(new RegExp(/%data%/g), resumeSections.projects.id).replace("%title%", resumeSections.projects.writtenForm));
+	$("#navbar_ul").append(navBarElement.replace(new RegExp(/%data%/g), resumeSections.education.id).replace("%title%", resumeSections.education.writtenForm));
+	$("#navbar_ul").append(navBarElement.replace(new RegExp(/%data%/g), resumeSections.googleMap.id).replace("%title%", resumeSections.googleMap.writtenForm));
+	//$("#navbar_ul").append(navBarElement.replace(new RegExp(/%data%/g), resumeSections.contactForm.id).replace("%title%", resumeSections.contactForm.writtenForm));
+}
+
+function displayNavContentBelowHeader() {
+	/*
+	This function creates containers linked to the tabs created in displayNavBarBelowHeader
+	and allocates relevant content to these containers by executing the previously written display functions
+	*/
+	$('#content').append(navContentStart);
+	var formattedNavContentActiveElement = navContentActiveElement.replace(new RegExp(/%data%/g), resumeSections.work.id);
+	$('#navcontent').append(formattedNavContentActiveElement);
+	$('#navcontent').append(navContentElement.replace(new RegExp(/%data%/g), resumeSections.projects.id));
+	$('#navcontent').append(navContentElement.replace(new RegExp(/%data%/g), resumeSections.education.id));
+	$('#navcontent').append(navContentElement.replace(new RegExp(/%data%/g), resumeSections.googleMap.id));
+	//$('#navcontent').append(navContentElement.replace(new RegExp(/%data%/g), resumeSections.contactForm.id));
+	bio.display();
+	work_experience.display();
+	projects.display();
+	education.display();
+	$("#mapDiv").append(googleMap);
+}
+
+displayNavBarBelowHeader();
+displayNavContentBelowHeader();
+
+// InitializeMap had to modified to work with bootstrap's tabs
+$("a[href='#mapDiv']").click(function() {
+    window.setTimeout(function() {
+        initializeMap();
+    },200);
+});
